3782890fbbb203a611d7b2b54ae325268575d5b4
Daichi Sakai <daisaru11@gmail.com>
2017-10-17 16:15:31 +0900

    Add named export

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import { LocalVideo } from './src/LocalVideo';
 import { RemoteVideo } from './src/RemoteVideo';
 import { Peer } from './src/Peer';
 
-const SkyWay = {
+export {
   Peer,
   LocalVideo,
   RemoteVideo,
 };
 
-export default SkyWay;
+export default {
+  Peer,
+  LocalVideo,
+  RemoteVideo,
+};
